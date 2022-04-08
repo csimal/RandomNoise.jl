3a9658b11f1206d378f67017f7daa80c199ebb11
csimal <cedric.simal@unamur.be>
2022-04-08 23:35:41 +0200

    added squirrel noise 5

---
+++ src/noise/squirrel5.jl
@@ -0,0 +1,35 @@
+
+"""
+    SquirrelNoise5
+
+The SquirrelNoise5 random noise function, originally by Squirrel Eiserloh.
+
+See http://eiserloh.net/noise/SquirrelNoise5.hpp for the original C++ code.
+"""
+struct SquirrelNoise5
+    seed::UInt32
+end
+
+function noise(n::Int32, s::SquirrelNoise5)
+    SQ5_BIT_NOISE1::UInt32 = 0xd2a80a3f
+    SQ5_BIT_NOISE2::UInt32 = 0xa884f197
+    SQ5_BIT_NOISE3::UInt32 = 0x6C736F4B
+    SQ5_BIT_NOISE4::UInt32 = 0xB79F3ABB
+    SQ5_BIT_NOISE5::UInt32 = 0x1b56c4f5
+
+    # NB. ⊻ == bitwise xor (\xor to get the symbol)
+    mangledbits = UInt32(n)
+    mangledbits *= SQ5_BIT_NOISE1
+    mangledbits += s.seed
+    mangledbits ⊻= (mangledbits >> 9)
+    mangledbits += SQ5_BIT_NOISE2
+    mangledbits ⊻= (mangledbits >> 11)
+    mangledbits *= SQ5_BIT_NOISE3
+    mangledbits ⊻= (mangledbits >> 13)
+    mangledbits += SQ5_BIT_NOISE4
+    mangledbits ⊻= (mangledbits >> 15)
+    mangledbits *= SQ5_BIT_NOISE5
+    mangledbits ⊻= (mangledbits >> 17)
+
+    return mangledbits
+end
