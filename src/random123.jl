3066f4480c9e031718fae3fdd3030e98e86b25e4
csimal <cedric.simal@unamur.be>
2022-04-23 21:42:59 +0200

    Added support for Random123

--- src/random123.jl
+++ src/random123.jl
@@ -1,7 +1,5 @@
-using Random123
-import Random123: AbstractR123
 
-function noise(n, rng::AbstractR123)
-    set_counter!(rng, n)
+function noise(n, rng::Random123.AbstractR123)
+    Random123.set_counter!(rng, n)
     Random123._value(rng)
 end
