272fed5c38be68f70978247a78197b13a284cbed
csimal <cedric.simal@unamur.be>
2022-07-03 20:09:22 +0200

    Factored out getindex mechanism

--- src/utils/transforms.jl
+++ src/utils/transforms.jl
@@ -1,4 +1,12 @@
 
+@inline function noise_getindex(rng, transform, i::Integer, ::Type{T}) where T
+    convert(T, transform(noise(i, rng)))
+end
+
+@inline function noise_getindex(rng, transform, i::Integer)
+    transform(noise(i, rng))
+end
+
 """
     NoiseUniform{T}
 
@@ -7,3 +15,7 @@ The generic transform for converting the default output of noise functions to un
 For floating point numbers, the values are uniformly distributed in the interval [0,1).
 """
 struct NoiseUniform{T} end
+
+@inline noise_getindex(rng, ::NoiseUniform{T}, i::Integer) where T = noise_convert(noise(i,rng), T)
+
+@inline noise_getindex(rng, t::NoiseUniform{T}, i::Integer, ::Type{T}) where T = noise_getindex(rng, t, i)
