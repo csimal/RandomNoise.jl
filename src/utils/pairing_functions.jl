3e6af8ff6d3a66cdb56fa29b6272624e844ad7b5
csimal <cedric.simal@unamur.be>
2022-07-03 20:08:45 +0200

    Added pairing functions

--- src/utils/pairing_functions.jl
+++ src/utils/pairing_functions.jl
@@ -6,17 +6,36 @@ The Hopcroft-Ullman pairing function, which bijectively maps two positive intege
 
 See https://mathworld.wolfram.com/PairingFunction.html
 """
-pairing2(i,j) = div((i+j-2)*(i+j-1),2) + i
+@inline pairing2(i,j) = div((i+j-2)*(i+j-1),2) + i
 
-pairing3(i,j,k) = pairing2(i,pairing2(j,k))
+@inline pairing3(i,j,k) = pairing2(pairing2(i,j),k)
 
-pairing4(i,j,k,l) = pairing2(pairing2(i,j),pairing2(k,l))
+@inline pairing4(i,j,k,l) = pairing2(pairing2(i,j),pairing2(k,l))
 
-pairing5(i,j,k,l,m) = pairing2(pairing2(i,j), pairing3(k,l,m))
+@inline pairing5(i,j,k,l,m) = pairing2(pairing2(i,j), pairing3(k,l,m))
 
-pairing6(i,j,k,l,m,n) = pairing2(pairing2(i,j), pairing4(k,l,m,n))
+@inline pairing6(i,j,k,l,m,n) = pairing2(pairing2(i,j), pairing4(k,l,m,n))
 
-pairing7(i,j,k,l,m,n,o) = pairing2(pairing3(i,j,k), pairing4(l,m,n,o))
+@inline pairing7(i,j,k,l,m,n,o) = pairing2(pairing3(i,j,k), pairing4(l,m,n,o))
 
-pairing8(i,j,k,l,m,n,o,p) = pairing2(pairing4(i,j,k,l), pairing4(m,n,o,p))
+@inline pairing8(i,j,k,l,m,n,o,p) = pairing2(pairing4(i,j,k,l), pairing4(m,n,o,p))
 
+function pairing(idx...)
+    x = [idx...]
+    n = length(x)
+    while n > 1
+        k = div(n,2)
+        for i in 1:k
+            x[i] = pairing2(x[2i-1], x[2i])
+        end
+        if isodd(n)
+            x[k+1] = x[n]
+            n = k+1
+        else
+            n = k
+        end
+    end
+    return x[1]
+end
+
+pairing(t::Tuple{Vararg{Integer,N}}) where N = pairing(t...)
--- test/pairing_functions.jl
+++ test/pairing_functions.jl
@@ -1,4 +1,70 @@
+using RandomNoise: pairing2, pairing3, pairing4, pairing5, pairing6, pairing7, pairing8, pairing
 
 @testset "Pairing Functions" begin
-    
+    @testset "pairing2" begin
+        @test typeof(pairing2(1,1)) == Int
+        @test pairing2(1,1) == 1
+        @test pairing2(1,2) == 2
+        @test pairing2(2,1) == 3
+    end
+    @testset "pairing3" begin
+        @test typeof(pairing3(1,1,1)) == Int
+        @test pairing3(1,1,1) == 1
+        @test pairing3(1,1,2) == 2
+        @test pairing3(2,1,1) == 6
+        @test pairing3(1,2,1) == 3
+    end
+    @testset "pairing4" begin
+        @test typeof(pairing4(1,1,1,1)) == Int
+        @test pairing4(1,1,1,1) == 1
+        @test pairing4(2,1,1,1) == 6
+        @test pairing4(1,2,1,1) == 3
+        @test pairing4(1,1,2,1) == 4
+        @test pairing4(1,1,1,2) == 2
+    end
+    @testset "pairing5" begin
+        @test typeof(pairing5(1,1,1,1,1)) == Int
+        @test pairing5(1,1,1,1,1) == 1
+        @test pairing5(2,1,1,1,1) == 6
+        @test pairing5(1,2,1,1,1) == 3
+        @test pairing5(1,1,2,1,1) == 16
+        @test pairing5(1,1,1,2,1) == 4
+        @test pairing5(1,1,1,1,2) == 2
+    end
+    @testset "pairing6" begin
+        @test typeof(pairing6(1,1,1,1,1,1)) == Int
+        @test pairing6(1,1,1,1,1,1) == 1
+        @test pairing6(2,1,1,1,1,1) == 6
+        @test pairing6(1,2,1,1,1,1) == 3
+        @test pairing6(1,1,2,1,1,1) == 16
+        @test pairing6(1,1,1,2,1,1) == 4
+        @test pairing6(1,1,1,1,2,1) == 7
+        @test pairing6(1,1,1,1,1,2) == 2
+    end
+    @testset "pairing7" begin
+        @test typeof(pairing7(1,1,1,1,1,1,1)) == Int
+        @test pairing7(1,1,1,1,1,1,1) == 1
+        @test pairing7(2,1,1,1,1,1,1) == 21
+        @test pairing7(1,2,1,1,1,1,1) == 6
+        @test pairing7(1,1,2,1,1,1,1) == 3
+        @test pairing7(1,1,1,2,1,1,1) == 16
+        @test pairing7(1,1,1,1,2,1,1) == 4
+        @test pairing7(1,1,1,1,1,2,1) == 7
+        @test pairing7(1,1,1,1,1,1,2) == 2
+    end
+    @testset "pairing8" begin
+        @test typeof(pairing8(1,1,1,1,1,1,1,1)) == Int
+        @test pairing8(1,1,1,1,1,1,1,1) == 1
+    end
+    @testset "pairing" begin
+        @test typeof(pairing(1,1)) == Int
+        @test pairing(1) == 1
+        @test pairing(1,1) == 1
+
+        @test pairing((1,1)) == pairing(1,1)
+
+        @test pairing(1,2) == pairing2(1,2)
+        @test pairing(1,2,3) == pairing3(1,2,3)
+        @test pairing(1,2,3,4) == pairing4(1,2,3,4)
+    end
 end
