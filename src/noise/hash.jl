6e53a91eadee873dad9a31c3dc6280588cce3844
csimal <cedric.simal@unamur.be>
2022-07-03 20:08:02 +0200

    Added doc examples

--- src/noise/hash.jl
+++ src/noise/hash.jl
@@ -8,6 +8,18 @@ This noise function depends on the implementation of `Base.hash` in the standard
 
 ## Fields
 `seed:UInt` seed of the noise function. Note that the constructor uses the hash of its argument to get the seed.
+
+## Examples
+```jldoctest
+julia> noise(5, HashNoise())
+0x63862ec0c596c950
+
+julia> hn = HashNoise(42)
+HashNoise(0x0f3db82f1e7b6f7a)
+
+julia> noise(5, hn)
+0x9d3bec003a77da92
+```
 """
 struct HashNoise <: AbstractNoise{UInt}
     seed::UInt
@@ -20,6 +32,6 @@ HashNoise() = HashNoise(UInt(0))
 
 @inline noise(n::UInt, h::HashNoise) = hash_noise(n, h.seed)
 
-function hash_noise(n::UInt, seed::UInt)
+@inline function hash_noise(n::UInt, seed::UInt)
     Base.hash(n, seed)
 end
