d89546ff6f81aa65fe2182e70ec89b0b5d4e359f
csimal <cedric.simal@unamur.be>
2022-11-10 15:44:37 +0100

    Add Aqua.jl to tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using Random123
 using Random
 using Distributions
 using Test
+using Aqua
 
 @testset "RandomNoise.jl" begin
     include("pairing_functions.jl")
@@ -11,4 +12,6 @@ using Test
     include("convert.jl")
     include("noise_rng.jl")
     include("noise_maps.jl")
+    include("transforms.jl")
+    Aqua.test_all(RandomNoise)
 end
